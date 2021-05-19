8ad5963ed4460fbc20e091582bef1b2bbef63464
vchealy <vchealy@gmail.com>
2021-05-19 15:20:11 +0100

    Writing XML Obj to JSON file

--- res.py
+++ res.py
@@ -2,12 +2,20 @@
 '''
 This module is for the management of the data retrieved from the other modules.
 
-Initially it is set to print:
+Initially it was set to print:
         stat_ status code reply from the GET response
+                This remains, because if there is an error this code could help
         inputer - The body of the respnse
 
-This will be changed to write to file rather than console.
+Write the inputer(XML object) to a JSON file rather than console.
 '''
+import xmltodict, json
+
 def result_manager(stat,inputer):
-    print(stat)
-    print(inputer)
+        obj = xmltodict.parse(inputer) # Convert XML input to JSON
+        obj2 = json.dumps(obj, indent=4, sort_keys=True) # Make it pretty JSON
+        with open('body.json', 'w') as f:
+                f.write(obj2)
+                
+        print(stat)
+
